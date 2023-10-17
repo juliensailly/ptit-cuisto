338ec875bd986a739ded999e34f8b076aa9944ef
mousse <guillaume.bergerot@etu.unicaen.fr>
2023-10-17 09:58:31 +0200

    refactor: homepage.php

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8">;     
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ptit Cuisto</title>
 </head>
 <body>
-    <!--<?php
+    <?php
     if (isset($_GET['id'])) {
         var_dump($_GET['id']);
     } else {
-        phpinfo();
+        header("Location: /View/homepage.php", true, 301);  
+        exit();
     }
-    ?>-->
+    ?>
 
-    
+
     
 </body>
 </html>
