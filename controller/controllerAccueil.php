8fb3ebca9f84e718bcc50d97c263a497ca93714d
Julien Sailly <juliensailly76@gmail.com>
2023-10-23 13:51:44 +0200

    feat: minor changes

--- controller/controllerAccueil.php
+++ controller/controllerAccueil.php
@@ -4,6 +4,7 @@ class controllerAccueil{
     $pageTitle = "Accueil";
     require(File::build_path(array("view", "navbar.php")));
     require(File::build_path(array("view/accueil", "accueil.php")));
+    phpinfo();
     require(File::build_path(array("view", "footer.php")));
   }
 }
--- controller/routeur.php
+++ controller/routeur.php
@@ -6,40 +6,37 @@ $controller = 'controllerAccueil';
 $action = 'readAll';
 
 /* Chargement du contrôleur */
-if(!isset($_GET['controller']) && !isset($_GET['action'])){
+if (!isset($_GET['controller']) && !isset($_GET['action'])) {
 	require(File::build_path(array("controller", "controllerAccueil.php")));
 	$controller::$action();
-}
-else{
-	if(isset($_GET['controller']) && !empty($_GET['controller'])){
-		$controller = 'controller'. ucfirst($_GET['controller']);
-
-		if(file_exists(File::build_path(array('controller', $controller . '.php')))){
-
-			if($controller!="controllerErreur"){
-				require(File::build_path(array('controller', $controller .  '.php')));
-			}
-
-			if(class_exists($controller)){
-				if(isset($_GET['action'])){
-					$action = $_GET['action'];
-				}
-				else if(isset($_POST['action'])){
-					$action = $_POST['action'];
-				}
-				$cl = get_class_methods($controller);
-
-				if(in_array($action, $cl)){
-					$controller::$action();
-				}
-				else{
-					controllerErreur::erreur("Action non existante");
-				}
-			}
-		}else{
-			controllerErreur::erreur("Le contrôleur n'existe pas");
-		}
-	}else{
+} else {
+	if (!isset($_GET['controller']) || empty($_GET['controller'])) {
 		controllerErreur::erreur("Le contrôleur n'a pas été défini ou champs vides");
+		return;
+	}
+	$controller = 'controller' . ucfirst($_GET['controller']);
+
+	if (!file_exists(File::build_path(array('controller', $controller . '.php')))) {
+		controllerErreur::erreur("Le contrôleur n'existe pas");
+		return;
+	}
+
+	if ($controller != "controllerErreur") {
+		require(File::build_path(array('controller', $controller . '.php')));
+	}
+
+	if (class_exists($controller)) {
+		if (isset($_GET['action'])) {
+			$action = $_GET['action'];
+		} else if (isset($_POST['action'])) {
+			$action = $_POST['action'];
+		}
+		$cl = get_class_methods($controller);
+
+		if (in_array($action, $cl)) {
+			$controller::$action();
+		} else {
+			controllerErreur::erreur("Action non existante");
+		}
 	}
 }
