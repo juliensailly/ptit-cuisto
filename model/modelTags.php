b86279113297538d89c8bc148ad27f5d13d2c29a
LucBUTGH <lucleveque14@outlook.fr>
2023-10-23 15:31:25 +0200

    feat(modelTags.php): add readTagsForOneRecipe function

--- model/modelTags.php
+++ model/modelTags.php
@@ -3,7 +3,6 @@ require_once(File::build_path(array("model", "model.php")));
 
 class modelTags {
   /** ATTRIBUTS **/
-  private $tag_id;
   private $tag_title;
 
   /** CONSTRUCTEUR **/
@@ -26,36 +25,45 @@ class modelTags {
 
   /** METHODES **/
   public static function readAll(){
-    $sql = "SELECT * FROM tags";
+    $sql = "SELECT * FROM tag";
     $rep = model::$pdo->query($sql);
     $rep->setFetchMode(PDO::FETCH_CLASS, 'modelTags');
     return $rep->fetchAll();
   }
 
   public function readTags(){
-    $sql="SELECT tag_title from tags";
+    $sql="SELECT tag_title from tag";
     $rep = model::$pdo->query($sql);
     $rep->setFetchMode(PDO::FETCH_CLASS, 'modelTags');
     return $rep->fetchAll();
 
   }
 
+  public function readTagsForOneRecipe($recipeId){
+    $sql="SELECT tag_title from tags_list WHERE rec_id = :rec_id";
+    $req_prep = model::$pdo->prepare($sql);
+    $req_prep->bindParam(':rec_id', $recipeId, PDO::PARAM_INT);
+    $req_prep->execute();
+    return $req_prep->fetchAll();
+
+  }
+
   public function create(){
-    $sql = "INSERT INTO types(idType, tag_title) VALUES (:id, :title)";
+    $sql = "INSERT INTO tag (idType, tag_title) VALUES (:id, :title)";
     $req_prep = model::$pdo->prepare($sql);
     $values = array("id" => NULL, "title" => $this->tag_title);
     $req_prep->execute($values);
   }
 
   public function update(){
-    $sql = 'UPDATE tags SET tag_title = :newTitle';
+    $sql = 'UPDATE tag SET tag_title = :newTitle';
     $req_prep = model::$pdo->prepare($sql);
     $values = array("newTitle" => $this->tag_title);
     $req_prep->execute($values);
   }
 
   public function delete(){
-    $sql = "DELETE FROM tags WHERE tag_title = :title";
+    $sql = "DELETE FROM tag WHERE tag_title = :title";
     $req_prep = model::$pdo->prepare($sql);
     $values = array("title" => $this->tag_title);
     $req_prep->execute($values);
