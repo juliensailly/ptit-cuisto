dde3cb5e54a5a25a1e63ff7298f169ba1cfebe31
Alenso Lopes <alenso.lopes@gmail.com>
2023-10-24 08:20:23 +0200

    feat: footer

--- view/footer.php
+++ view/footer.php
@@ -1,5 +1,46 @@
-        <footer>
-            
-        </footer>
-    </body>
+<footer class=" text-center text-white" style="background-color: #2a3990;">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+        <!-- Section: Social media -->
+        <section class="mb-4">
+            <!-- Facebook -->
+            <a class="btn text-white btn-floating m-1" style="background-color: #3b5998; border:1px solid black;"
+                href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+
+            <!-- Twitter -->
+            <a class="btn text-white btn-floating m-1" style="background-color: #55acee; border:1px solid black;"
+                href="#!" role="button"><i class="fab fa-twitter"></i></a>
+
+            <!-- Instagram -->
+            <a class="btn text-white btn-floating m-1" style="background-color: #ac2bac; border:1px solid black;"
+                href="#!" role="button"><i class="fab fa-instagram"></i></a>
+
+            <!-- Linkedin -->
+            <a class="btn text-white btn-floating m-1" style="background-color: #0082ca; border:1px solid black;"
+                href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+        </section>
+        <!-- Section: Social media -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" id="copyright">
+        <script>
+            document.getElementById('copyright').innerHTML = '&copy ' + new Date().getFullYear();
+        </script>
+        <a href='https://github.com/juliensailly' target="_blank" class='text-white'>Julien Sailly,</a>
+        <a href='https://github.com/moustave' target="_blank" class='text-white'>Guillaume Bergerot,</a>
+        <a href='https://github.com/LucBUTGH' target="_blank" class='text-white'>Luc Leveque,</a>
+        <a href='https://github.com/AlensoLopes' target="_blank" class='text-white'>Alenso Lopes.</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+</body>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/880ee86f7a.js" crossorigin="anonymous"></script>
+
 </html>
