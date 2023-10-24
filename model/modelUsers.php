9039b4e68391f8318e377f9c8a43e880f27ae456
LucBUTGH <lucleveque14@outlook.fr>
2023-10-24 11:25:48 +0200

    fix: users pdo

--- model/modelUsers.php
+++ model/modelUsers.php
@@ -13,7 +13,7 @@ class modelIngredients extends model {
   public function create($email, $password, $lastname, $name, $date){
     $sql = "INSERT INTO " . $this->table . " (users_id, users_email, users_password, users_lastname, users_name, users_inscription_date, users_type, users_status) 
     VALUES (:id, :email, :password, :lastname, :name, :date, :type, :status)";
-    $req_prep = model::$pdo->prepare($sql);
+    $req_prep = $this->pdo->prepare($sql);
     $req_prep->bindParam(':id', NULL, PDO::PARAM_INT);
     $req_prep->bindParam(':email', $email, PDO::PARAM_STR);
     $req_prep->bindParam(':password', $password, PDO::PARAM_STR);
@@ -27,7 +27,7 @@ class modelIngredients extends model {
 
   public function updateLastname($newLName) {
     $sql = "UPDATE " . $this->table . " SET users_lastname = :new WHERE users_id = :id";
-    $req_prep = model::$pdo->prepare($sql);
+    $req_prep = $this->pdo->prepare($sql);
     $req_prep->bindParam(':id', $this->id, PDO::PARAM_INT);
     $req_prep->bindParam(':new', $newLName, PDO::PARAM_STR);
     $req_prep->execute();
@@ -36,7 +36,7 @@ class modelIngredients extends model {
 
   public function updateName($newName) {
     $sql = "UPDATE " . $this->table . " SET users_name = :new WHERE users_id = :id";
-    $req_prep = model::$pdo->prepare($sql);
+    $req_prep = $this->pdo->prepare($sql);
     $req_prep->bindParam(':id', $this->id, PDO::PARAM_INT);
     $req_prep->bindParam(':new', $newName, PDO::PARAM_STR);
     $req_prep->execute();
@@ -45,7 +45,7 @@ class modelIngredients extends model {
 
   public function updateEmail($newEmail){
     $sql = "UPDATE " . $this->table . " SET users_email = :new WHERE users_id = :id";
-    $req_prep = model::$pdo->prepare($sql);
+    $req_prep = $this->pdo->prepare($sql);
     $req_prep->bindParam(':id', $this->id, PDO::PARAM_INT);
     $req_prep->bindParam(':new', $newEmail, PDO::PARAM_STR);
     $req_prep->execute();
@@ -54,7 +54,7 @@ class modelIngredients extends model {
 
   public function updatePassword($newPassword){
     $sql = "UPDATE " . $this->table . " SET users_password = :new WHERE users_id = :id";
-    $req_prep = model::$pdo->prepare($sql);
+    $req_prep = $this->pdo->prepare($sql);
     $req_prep->bindParam(':id', $this->id, PDO::PARAM_INT);
     $req_prep->bindParam(':new', $newPassword, PDO::PARAM_STR);
     $req_prep->execute();
@@ -62,8 +62,9 @@ class modelIngredients extends model {
   }
 
   public function delete(){
-    $sql = "DELETE FROM " . $this->table . " WHERE users_id = " .$this->id;
-    $req_prep = model::$pdo->prepare($sql);
+    $sql = "DELETE FROM " . $this->table . " WHERE users_id = :id";
+    $req_prep = $this->pdo->prepare($sql);
+    $req_prep->bindParam(':id', $this->id, PDO::PARAM_INT);
     $req_prep->execute();
     return $query->execute();
   }
