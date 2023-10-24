f97ad7d49c7397d7098ed051e916e39e5feee7ee
LucBUTGH <lucleveque14@outlook.fr>
2023-10-24 11:20:23 +0200

    fix: ingredients pdo

--- model/modelIngredients.php
+++ model/modelIngredients.php
@@ -12,33 +12,32 @@ class modelIngredients extends model {
 
   public function create($nomIngredient, $desc){
     $sql = "INSERT INTO " . $this->table . " (ing_id, ing_title, ing_desc) VALUES (:id, :nom, :desc)";
-    $req_prep = model::$pdo->prepare($sql);
+    $req_prep = $this->pdo->prepare($sql);
     $req_prep->bindParam(':id', NULL, PDO::PARAM_NULL);
     $req_prep->bindParam(':nom', $nomIngredient, PDO::PARAM_STR);
     $req_prep->bindParam(':desc', $desc, PDO::PARAM_STR);
     $req_prep->execute();
   }
 
-  public function updateIngredient($oldIngredient, $newIngredient) {
-    $sql = "UPDATE " . $this->table . " SET nomType = :new WHERE nomType = :old";
-    $req_prep = model::$pdo->prepare($sql);
-    $req_prep->bindParam(':old', $oldIngredient, PDO::PARAM_STR);
-    $req_prep->bindParam(':new', $newIngredient, PDO::PARAM_STR);
+  public function update($newTitle) {
+    $sql = "UPDATE " . $this->table . " SET ing_title = :new WHERE ing_id = :id";
+    $req_prep = $this->pdo->prepare($sql);
+    $req_prep->bindParam(':id', $this->id, PDO::PARAM_INT);
+    $req_prep->bindParam(':new', $newTitle, PDO::PARAM_STR);
     $req_prep->execute();
-    return $req_prep->rowCount() > 0;
   }
 
-  public function updateDesc($ingredient, $newDesc) {
-    $sql = "UPDATE " . $this->table . " SET ing_desc = :new WHERE ing_title = :ing";
-    $req_prep = model::$pdo->prepare($sql);
-    $req_prep->bindParam(':ing', $ingredient, PDO::PARAM_STR);
+  public function updateDesc($newDesc) {
+    $sql = "UPDATE " . $this->table . " SET ing_desc = :new WHERE ing_id = :id";
+    $req_prep = $this->pdo->prepare($sql);
+    $req_prep->bindParam(':id', $this->id, PDO::PARAM_INT);
     $req_prep->bindParam(':new', $newDesc, PDO::PARAM_STR);
     $req_prep->execute();
   }
 
   public function delete($nomIngredient){
     $sql = "DELETE FROM " . $this->table . " WHERE ing_title = :nom";
-    $req_prep = model::$pdo->prepare($sql);
+    $req_prep = $this->pdo->prepare($sql);
     $req_prep->bindParam(':nom', $nomIngredient, PDO::PARAM_STR);
     $req_prep->execute();
   }
