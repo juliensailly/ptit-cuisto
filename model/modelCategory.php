258bfaf9efc580f0a8ca8c6e5c3fd2e7bc361336
LucBUTGH <lucleveque14@outlook.fr>
2023-10-24 11:24:37 +0200

    fix: category pdo

--- model/modelCategory.php
+++ model/modelCategory.php
@@ -12,26 +12,26 @@ class modelIngredients extends model {
 
   public function create($nomCategory, $desc){
     $sql = "INSERT INTO " . $this->table . " (cat_id, cat_title, cat_desc) VALUES (:id, :title, :desc)";
-    $req_prep = model::$pdo->prepare($sql);
+    $req_prep = $this->pdo->prepare($sql);
     $req_prep->bindParam(':id', NULL, PDO::PARAM_INT);
     $req_prep->bindParam(':title', $nomCategory, PDO::PARAM_STR);
     $req_prep->bindParam(':desc', $desc, PDO::PARAM_STR);
     return $req_prep->execute();
   }
 
-  //En cours (clé étrangère users_id ?)
   public function update($newTitle) {
     $sql = "UPDATE " . $this->table . " SET cat_title = :new WHERE cat_id = :id";
-    $req_prep = model::$pdo->prepare($sql);
+    $req_prep = $this->pdo->prepare($sql);
     $req_prep->bindParam(':id', $this->id, PDO::PARAM_INT);
-    $req_prep->bindParam(':new', $newDesc, PDO::PARAM_STR);
+    $req_prep->bindParam(':new', $newTitle, PDO::PARAM_STR);
     $req_prep->execute();
     return $req_prep->rowCount() > 0;
   }
 
   public function delete(){
-    $sql = "DELETE FROM " . $this->table . " WHERE cat_id = " .$this->id;
-    $req_prep = model::$pdo->prepare($sql);
+    $sql = "DELETE FROM " . $this->table . " WHERE cat_id = :id";
+    $req_prep = $this->pdo->prepare($sql);
+    $req_prep->bindParam(':id', $this->id, PDO::PARAM_INT);
     $req_prep->execute();
     return $query->execute();
   }
