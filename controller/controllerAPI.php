bcda37e1d700b5b5c756f028cc8fc55aa746489d
Julien Sailly <juliensailly76@gmail.com>
2023-10-29 22:01:58 +0100

    feat: api working

--- controller/controllerAPI.php
+++ controller/controllerAPI.php
@@ -7,7 +7,8 @@ class controllerAPI{
     public static function filtre() {
         if (!isset($_GET["cat_id"])) return;
         $cat_id = $_GET["cat_id"];
-        $tab_t = modelAPI::getRecipesByCategories($cat_id);
-        echo json_encode($tab_t);
+        $data = modelAPI::getRecipesByCategories($cat_id);
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($data);
     }
 }
