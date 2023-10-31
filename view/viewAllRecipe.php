b56c729cdb316db3a6aa26c5ad3543ae40047b5f
Julien Sailly <juliensailly76@gmail.com>
2023-11-01 00:57:35 +0100

    feat: recipe pages navigation added

--- view/viewAllRecipe.php
+++ view/viewAllRecipe.php
@@ -3,7 +3,7 @@
 
     <?php
     foreach ($recipes as $key => $recipe) {
-        if ($key%2 == 0) {
+        if ($key % 2 == 0) {
             echo "<div class='recipes_row'>";
         }
         ?>
@@ -23,10 +23,44 @@
         </div>
 
         <?php
-        if ($key%2 != 0) {
+        if ($key % 2 != 0) {
             echo "</div>";
         }
+        if ($key == sizeof($recipes) - 1) {
+            echo "<div class=\"card\" style=\"visibility:hidden;\"></div></div>";
+        }
+    }
+    if (sizeof($recipes) == 0) {
+        echo "<div class='alert alert-warning' role='alert'>Aucune recette</div>";
     }
     ?>
+</div>
 
+<div class="page_number btn-group">
+    <?php
+    if ($page > 1) {
+        ?>
+        <a href="index.php?controller=recipes&action=readAll&page=<?= $page - 1 ?>" class="btn btn-outline-primary"><i
+                class="fa-solid fa-arrow-left"></i></a>
+        <?php
+    } else {
+        ?>
+        <button type="button" class="btn btn-outline-primary" disabled><i class="fa-solid fa-arrow-left"></i></button>
+        <?php
+    }
+    ?>
+    <span class="btn btn-outline-primary">
+        <?php
+        if (isset($_GET['page'])) {
+            if ($_GET['page'] != "")
+                echo "Page " . $_GET['page'];
+            else
+                echo "Page 1";
+        } else {
+            echo "Page 1";
+        }
+        ?>
+    </span>
+    <a href="index.php?controller=recipes&action=readAll&page=<?= $page + 1 ?>" class="btn btn-outline-primary"><i
+            class="fa-solid fa-arrow-right"></i></a>
 </div>
