9bef02a0bec31c177f4643cd9122ef402faa2a78
Julien Sailly <juliensailly76@gmail.com>
2023-10-31 13:24:25 +0100

    feat: skeleton of the recipe view

--- view/viewRecipe.php
+++ view/viewRecipe.php
@@ -1,1 +1,117 @@
-<h2><?= $recipe['rec_title'] ?></h2>
+<h2>Titre de la recette</h2>
+<h4><i>Catégorie</i></h4>
+
+<article id="recipe_view">
+    <div class="column">
+        <div class="img_container">
+            <img src="https://picsum.photos/200/300" alt="Image de la recette">
+        </div>
+        <div class="author_save">
+            <div class="author">
+                <img src="https://picsum.photos/50/50" alt="PP">
+                <div>
+                    <p>Michel</p>
+                    <p>Publié le 01/01/2021</p>
+                </div>
+            </div>
+            <form method="POST" action="" class="save_recipe">
+                <input type="submit" value="Sauvegarder">
+                <span>3</span>
+                <i class="fa-regular fa-heart" style="color: #ffffff;"></i>
+            </form>
+        </div>
+        <div class="tags">
+            <h3>Tags</h3>
+            <div>
+                <span>Tag</span>
+                <span>Tag plus long</span>
+                <span>Tag moyen</span>
+                <span>Tag moyen</span>
+                <span>Tag plus long</span>
+            </div>
+        </div>
+        <div class="comments">
+            <h3>Commentaires</h3>
+            <div>
+                <div class="comment">
+                    <div class="author">
+                        <img src="https://picsum.photos/50/50" alt="PP">
+                        <div>
+                            <p>Michel</p>
+                            <p>Publié le 01/01/2021</p>
+                        </div>
+                    </div>
+                    <p>it amen eslorem ipsum sit amen es lorem ipsum dolor sit
+                        amen eslorem sit ipsum dolor sit amen eslorem ipsum sit
+                        amen eslorem ipsum.
+                    </p>
+                </div>
+                <hr>
+                <div class="comment">
+                    <div class="author">
+                        <img src="https://picsum.photos/50/50" alt="PP">
+                        <div>
+                            <p>Michel</p>
+                            <p>Publié le 01/01/2021</p>
+                        </div>
+                    </div>
+                    <p>it amen eslorem ipsum sit amen es lorem ipsum dolor sit
+                        amen eslorem sit ipsum dolor sit amen eslorem ipsum sit
+                        amen eslorem ipsum.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="column">
+        <div class="recipe_content">
+            <h3>Indications</h3>
+            <p>
+                1 - Faites les boulettes
+                2 - Faites cuire les carottes
+                3 - Mélangez les deux
+                4 - Servez
+
+                lorem ipsum dolor sit amen eslorem sit ipsum
+                dolor sit amen eslorem ipsum sit amen es lorem
+                ipsum dolor sit amen eslorem sit ipsum dolor sit
+                amen eslorem ipsum sit amen es lorem ipsum
+                dolor sit amen eslorem sit ipsum dolor sit amen
+                eslorem ipsum sit amen eslorem ipsum dolor sit
+                amen eslorem sit ipsum dolor sit amen eslorem
+                ipsum sit amen eslorem ipsum dolor sit amen
+                eslorem sit ipsum dolor sit amen eslorem ipsum
+                sit amen es
+            </p>
+        </div>
+        <div class="ingredients">
+            <div>
+                <h3>Ingrédients</h3>
+                <div class="person_number btn-group">
+                    <button type="button" class="btn btn-outline-primary">-</button>
+                    <span class="btn btn-outline-primary">Personnes</span>
+                    <button type="button" class="btn btn-outline-primary">+</button>
+                </div>
+            </div>
+            <table>
+                <tr>
+                    <th>Ingrédient</th>
+                    <th>Quantité</th>
+                </tr>
+                <tr>
+                    <td>Ingrédient 1</td>
+                    <td>Quantité 1</td>
+                </tr>
+                <tr>
+                    <td>Ingrédient 2</td>
+                    <td>Quantité 2</td>
+                </tr>
+                <tr>
+                    <td>Ingrédient 3</td>
+                    <td>Quantité 3</td>
+                </tr>
+            </table>
+
+        </div>
+    </div>
+</article>
