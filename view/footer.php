a03fde2abf5eeb6dfdce3de141b5d8389d5b677e
Julien Sailly <juliensailly76@gmail.com>
2023-11-01 17:33:30 +0100

    fix: html validator passed

--- view/footer.php
+++ view/footer.php
@@ -1,30 +1,21 @@
 </div>
 <footer class=" text-center text-white" style="background-color: #2a3990;">
-    <!-- Grid container -->
     <div class="container p-4 pb-0">
-        <!-- Section: Social media -->
-        <section class="mb-4">
-            <!-- Facebook -->
+        <div class="mb-4">
             <a class="btn text-white btn-floating m-1" style="background-color: #3b5998; border:1px solid black;"
                 href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
 
-            <!-- Twitter -->
             <a class="btn text-white btn-floating m-1" style="background-color: #55acee; border:1px solid black;"
                 href="#!" role="button"><i class="fab fa-twitter"></i></a>
 
-            <!-- Instagram -->
             <a class="btn text-white btn-floating m-1" style="background-color: #ac2bac; border:1px solid black;"
                 href="#!" role="button"><i class="fab fa-instagram"></i></a>
 
-            <!-- Linkedin -->
             <a class="btn text-white btn-floating m-1" style="background-color: #0082ca; border:1px solid black;"
                 href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
-        </section>
-        <!-- Section: Social media -->
+</div>
     </div>
-    <!-- Grid container -->
 
-    <!-- Copyright -->
     <div class="text-center p-3" id="copyright">
         <script>
             document.getElementById('copyright').innerHTML = '&copy ' + new Date().getFullYear();
@@ -34,7 +25,6 @@
         <a href='https://github.com/LucBUTGH' target="_blank" class='text-white'>Luc Leveque,</a>
         <a href='https://github.com/AlensoLopes' target="_blank" class='text-white'>Alenso Lopes.</a>
     </div>
-    <!-- Copyright -->
 </footer>
 <script src="resources/script/bootstrap.bundle.min.js"></script>
 <script src="https://kit.fontawesome.com/880ee86f7a.js" crossorigin="anonymous"></script>
