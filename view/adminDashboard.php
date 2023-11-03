e11a40d7a3c1aeeb0fe1c076fa6d4fd758212b27
Julien Sailly <juliensailly76@gmail.com>
2023-11-03 22:44:45 +0100

    feat: recipe responsive design

--- view/adminDashboard.php
+++ view/adminDashboard.php
@@ -27,55 +27,71 @@
 </ul>
 <div class="tab-content" id="adminTabsContent">
     <div class="tab-pane fade show active" id="awaiting-recipe" role="tabpanel" aria-labelledby="awaiting-recipe-tab">
-        <?php
-        foreach ($awaitingRecipes as $index => $recipe) { ?>
-            <div class="card">
-                <img class="card-img-top" src="<?= $recipe_img_path . $recipe->rec_image_src ?>"
-                    alt="Illustration de <?= $recipe->rec_title ?>">
-                <div class="card-body">
-                    <h4 class="card-title">
-                        <?= $recipe->rec_title ?>
-                    </h4>
-                    <h6 class="recipe_category card-subtitle">
-                        <a href="index.php?controller=filtre&action=categories&id=<?= $recipe->cat_id ?>"><i>
-                                <?= $recipe->cat_title ?>
-                            </i></a>
-                    </h6>
-                    <p class="card-text">
-                        <?= $recipe->rec_summary ?>
-                    </p>
+        <h3>Recettes en attente de validation</h3>
+        <div class="tabContentContainer">
+            <?php
+            foreach ($awaitingRecipes as $index => $recipe) { ?>
+                <div class="card">
+                    <img class="card-img-top" src="<?= $recipe_img_path . $recipe->rec_image_src ?>"
+                        alt="Illustration de <?= $recipe->rec_title ?>">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <?= $recipe->rec_title ?>
+                        </h4>
+                        <h6 class="recipe_category card-subtitle">
+                            <a href="index.php?controller=filtre&action=categories&id=<?= $recipe->cat_id ?>"><i>
+                                    <?= $recipe->cat_title ?>
+                                </i></a>
+                        </h6>
+                        <p class="card-text">
+                            <?= $recipe->rec_summary ?>
+                        </p>
+                    </div>
+                    <div class="btn-group">
+                        <a href="index.php?controller=recipes&action=deleteForm&id=<?= $recipe->rec_id ?>"
+                            class="btn btn-outline-danger">Supprimer la recette</a>
+                        <a href="index.php?controller=recipes&action=read&id=<?= $recipe->rec_id ?>"
+                            class="btn btn-primary">Plus de détails</a>
+                        <a href="index.php?controller=admin&action=validRecipe&id=<?= $recipe->rec_id ?>"
+                            class="btn btn-outline-success">Accepter la recette</a>
+                    </div>
                 </div>
-                <div class="btn-group">
-                    <a href="index.php?controller=recipes&action=deleteForm&id=<?= $recipe->rec_id ?>"
-                        class="btn btn-outline-danger">Supprimer la recette</a>
-                    <a href="index.php?controller=recipes&action=read&id=<?= $recipe->rec_id ?>"
-                    class="btn btn-primary">Plus de détails</a>
-                    <a href="index.php?controller=admin&action=validRecipe&id=<?= $recipe->rec_id ?>"
-                        class="btn btn-outline-success">Accepter la recette</a>
-                </div>
-            </div>
-            <?php
-        }
-        if (sizeof($awaitingRecipes) % 2 != 0) { ?>
-            <div class="card" style="visibility:hidden;"></div>
-            <?php
-        }
-        if (empty($awaitingRecipes)) { ?>
-            <div class="alert alert-secondary" role="alert">Aucune recette en attente</div>
-            <?php
-        }
-        ?>
+                <?php
+            }
+            if (sizeof($awaitingRecipes) % 2 != 0) { ?>
+                <div class="card" style="visibility:hidden;"></div>
+                <?php
+            }
+            if (empty($awaitingRecipes)) { ?>
+                <div class="alert alert-secondary" role="alert">Aucune recette en attente</div>
+                <?php
+            }
+            ?>
+        </div>
+
     </div>
     <div class="tab-pane fade" id="awaiting-comment" role="tabpanel" aria-labelledby="awaiting-comment-tab">
+        <h3>Commentaires en attente de validation</h3>
+        <div class="tabContentContainer">
 
+        </div>
     </div>
     <div class="tab-pane fade" id="edito" role="tabpanel" aria-labelledby="edito-tab">
+        <h3>Modification de l'éditorial de la page d'accueil</h3>
+        <div class="tabContentContainer">
 
+        </div>
     </div>
     <div class="tab-pane fade" id="users" role="tabpanel" aria-labelledby="users-tab">
+        <h3>Utilisateurs de Pti-Cuisto</h3>
+        <div class="tabContentContainer">
 
+        </div>
     </div>
     <div class="tab-pane fade" id="recipes" role="tabpanel" aria-labelledby="recipes-tab">
+        <h3>Recettes de Pti-Cuisto</h3>
+        <div class="tabContentContainer">
 
+        </div>
     </div>
 </div>
