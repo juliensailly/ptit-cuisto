f3262da76c141c41e1328874fd1fcee96d2e634d
Alenso Lopes <alenso.lopes@gmail.com>
2023-11-06 15:05:15 +0100

    feat: ajout visualisation profile

--- model/modelAccount.php
+++ model/modelAccount.php
@@ -29,4 +29,33 @@ class modelAccount {
     $req_prep->setFetchMode(PDO::FETCH_CLASS, 'model');
     return $req_prep->fetch();
   }
+
+  public static function getUsersRecipes($users_id)
+    {
+        $model = new model();
+        $model->init();
+        $sql = "SELECT rec_id, rec_title, cat_id, cat_title, rec_summary, rec_image_src, rec_creation_date, rec_modification_date from recipes
+        join category using(cat_id)
+        join users using(users_id)
+        where users_id = :users_id";
+        $req_prep = $model::$pdo->prepare($sql);
+        $req_prep->bindParam(':users_id', $users_id);
+        $req_prep->execute();
+        $req_prep->setFetchMode(PDO::FETCH_CLASS, 'model');
+        return $req_prep->fetchAll();
+    }
+
+  public static function getUsersLikedRecipes($userId){
+    $model = new model();
+    $model->init();
+    $sql = "SELECT rec_id, rec_title, cat_id, cat_title, rec_summary, rec_image_src, rec_creation_date, rec_modification_date from recipes
+    join category using(cat_id)
+    join likes using(rec_id)
+    where likes.users_id = :users_id";
+    $req_prep = $model::$pdo->prepare($sql);
+    $req_prep->bindParam(':users_id', $userId);
+    $req_prep->execute();
+    $req_prep->setFetchMode(PDO::FETCH_CLASS, 'model');
+    return $req_prep->fetchAll();
+  }
 }
---
+++ view/accountView.php
@@ -0,0 +1,96 @@
+<?php global $recipe_img_path; ?>
+
+<h2>Tableau de bord - Utilisateurs</h2>
+
+<section class="userDashboard">
+    <h3>Utilisateurs</h3>
+    <table>
+        <thead>
+            <tr>
+                <th>Identifiant</th>
+                <th>Adresse mail</th>
+                <th>Prénom</th>
+                <th>Nom</th>
+                <th>Pseudo</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+              $users = $_SESSION['login'];
+              echo '<tr>';
+              echo '<td>' . $users->users_id . '</td>';
+              echo '<td>' . $users->users_email . '</td>';
+              echo '<td>' . $users->users_name . '</td>';
+              echo '<td>' . $users->users_lastname . '</td>';
+              echo '<td>' . $users->users_pseudo . '</td>';
+              echo '</tr>';
+              echo '<td>';
+              echo '</td>';
+              echo '</tr>';
+            ?>
+        </tbody>
+    </table>
+</section>
+
+<div class="tab-content" id="adminTabsContent">
+    <div class="tab-pane fade show active" id="awaiting-recipe" role="tabpanel" aria-labelledby="awaiting-recipe-tab">
+        <h3>Recettes écrite </h3>
+        <div class="tabContentContainer">
+            <?php
+            foreach ($usersRecipes as $index => $recipe) { ?>
+                <div class="card">
+                    <img class="card-img-top" src="<?= $recipe_img_path . $recipe->rec_image_src ?>"
+                        alt="Illustration de <?= $recipe->rec_title ?>">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <?= $recipe->rec_title ?>
+                        </h4>
+                        <h6 class="recipe_category card-subtitle">
+                            <a href="index.php?controller=filtre&action=categories&id=<?= $recipe->cat_id ?>"><i>
+                                    <?= $recipe->cat_title ?>
+                                </i></a>
+                        </h6>
+                        <p class="card-text">
+                            <?= $recipe->rec_summary ?>
+                        </p>
+                    </div>
+                    
+                </div>
+                <?php
+            }
+            ?>
+
+    </div>
+    </div>
+
+    <div class="tab-content" id="adminTabsContent">
+    <div class="tab-pane fade show active" id="awaiting-recipe2" role="tabpanel" aria-labelledby="awaiting-recipe-tab">
+        <h3>Recettes favorites</h3>
+        <div class="tabContentContainer">
+            <?php
+            foreach ($usersLikedRecipes as $index => $recipe) { ?>
+                <div class="card">
+                    <img class="card-img-top" src="<?= $recipe_img_path . $recipe->rec_image_src ?>"
+                        alt="Illustration de <?= $recipe->rec_title ?>">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <?= $recipe->rec_title ?>
+                        </h4>
+                        <h6 class="recipe_category card-subtitle">
+                            <a href="index.php?controller=filtre&action=categories&id=<?= $recipe->cat_id ?>"><i>
+                                    <?= $recipe->cat_title ?>
+                                </i></a>
+                        </h6>
+                        <p class="card-text">
+                            <?= $recipe->rec_summary ?>
+                        </p>
+                    </div>
+                    
+                </div>
+                <?php
+            }
+            ?>
+
+    </div>
+    </div>
+
